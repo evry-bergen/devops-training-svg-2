640f8e34967a60e61f1774258c2c72c46567bdc1
Shamrez Iqbal <shamrez.iqbal@evry.com>
2020-02-18 10:46:41 +0100

    initial commit du lissom

---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "google" {
+    project = "svg-devops-training-2"
+    region = "europe-north1"
+    zone = "europe-north1-a"
+
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+
+    name = "my-instance"
+    machine_type = "g1-small"
+    boot_disk {
+        initialize_params {
+            image = "ubuntu-os-cloud/ubuntu-1804-lts"
+        }
+    }
+    network_interface  {
+         network = "${google_compute_network.vpc_network.self_link}"
+         subnetwork = "test-subnetwork"
+    }
+
+    metadata_startup_script = "sudo apt get -y update && sudo apt-get -y install nginx && sudo service nginx start"
+    
+
+}
+
+
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = "europe-north1"
+  network       = google_compute_network.vpc_network.self_link
+ 
+}
+
+resource "google_compute_network" "vpc_network" {
+   name = "my-name"
+   auto_create_subnetworks = "false"
+}
