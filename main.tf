7950c0f452eaea3a7f94387834cae5a640384377
Shamrez Iqbal <shamrez.iqbal@evry.com>
2020-02-20 09:52:28 +0100

    format

--- main.tf
+++ main.tf
@@ -1,34 +1,34 @@
 terraform {
   backend "gcs" {
-    bucket  = "tf-state-prod-shamglam"
-    prefix  = "terraform/state"
+    bucket = "tf-state-prod-shamglam"
+    prefix = "terraform/state"
   }
 }
 
 provider "google" {
-    project = "svg-devops-training-2"
-    region = "europe-north1"
-    zone = "europe-north1-a"
+  project = "svg-devops-training-2"
+  region  = "europe-north1"
+  zone    = "europe-north1-a"
 
 }
 
 
 resource "google_compute_instance" "vm_instance" {
 
-    name = "my-instance"
-    machine_type = "g1-small"
-    boot_disk {
-        initialize_params {
-            image = "ubuntu-os-cloud/ubuntu-1804-lts"
-        }
+  name         = "my-instance"
+  machine_type = "g1-small"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804-lts"
     }
-    network_interface  {
-         network = "${google_compute_network.vpc_network.self_link}"
-         subnetwork = "test-subnetwork"
-    }
+  }
+  network_interface {
+    network    = "${google_compute_network.vpc_network.self_link}"
+    subnetwork = "test-subnetwork"
+  }
 
-    metadata_startup_script = "sudo apt get -y update && sudo apt-get -y install nginx && sudo service nginx start"
-    
+  metadata_startup_script = "sudo apt get -y update && sudo apt-get -y install nginx && sudo service nginx start"
+
 
 }
 
@@ -38,10 +38,10 @@ resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" 
   ip_cidr_range = "10.0.0.0/24"
   region        = "europe-north1"
   network       = google_compute_network.vpc_network.self_link
- 
+
 }
 
 resource "google_compute_network" "vpc_network" {
-   name = "my-name"
-   auto_create_subnetworks = "false"
+  name                    = "my-name"
+  auto_create_subnetworks = "false"
 }
