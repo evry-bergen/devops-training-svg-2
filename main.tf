6d7ee00479f06b93e19005e2a34201a38ce1e297
supershammy <shampo@hotmail.com>
2020-03-08 21:16:09 +0100

    added subnetwork

--- main.tf
+++ main.tf
@@ -21,6 +21,14 @@ resource "google_compute_network" "vpc_network" {
 }
 
 
+resource "google_compute_subnetwork" "network-with-private-secondary-ip-ranges" {
+  name          = "test-subnetwork"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = "europe-north1"
+  network       = google_compute_network.vpc_network.self_link
+
+}
+
 resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
   location = "europe-north1"
